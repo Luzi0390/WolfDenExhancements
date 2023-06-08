1164d93a3336745832d0a0aab552bdbbebd3b28b
Luzijun0390 <luzijun3838300@163.com>
2023-06-08 21:48:47 +0800

    关联油猴！

---
+++ WDE.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Wolf Den Exhancements
+// @namespace    WDE
+// @version      0.0.3
+// @description  狼窝用
+// @author       Luzi
+// @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
+// @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+
+
+(function() {
+  'use strict';
+
+  const script = document.createElement("script");
+script.src = `https://luzi0390.github.io/WolfDenExhancements/main.js`;
+  window.s = script;
+document.head.appendChild(script);
+})();
